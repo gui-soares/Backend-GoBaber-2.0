6574fec217b1b22f0239a837c99a11702ffda110
gui-soares <eng.guilhermemsoares@gmail.com>
2019-07-25 20:58:51 -0300

    Autenticação JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '2d3fbd8b1db6a1f3441f5277898055ac',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 const routes = new Router();
 
+// Users
 routes.post('/users', UserController.store);
+
+// Sessions
+routes.post('/sessions', SessionController.store);
 
 export default routes;
