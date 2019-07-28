8bc959fc7ae3aa140da111af9a5113e79375817d
gui-soares <eng.guilhermemsoares@gmail.com>
2019-07-28 13:52:28 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a0bcc5dfbdf2a6',
+    pass: 'd7da415dd8cf6f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
