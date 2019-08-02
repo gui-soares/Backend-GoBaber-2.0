cfa3f02d517a4693ee5c30ffa7c5941488aedb75
gui-soares <eng.guilhermemsoares@gmail.com>
2019-08-02 20:44:19 -0300

    fix: .env & AppointmentController

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -12,7 +12,7 @@ import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
-    const { page = 1 } = req.query;
+    const page = req.query.page || 1;
 
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
